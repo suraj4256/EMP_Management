8d83a97f7bf153de28592817927ab153f9030d29
Suraj Dey <surajdey2k1@gmail.com>
2024-12-01 02:21:23 +0530

    Company Login controller setup

--- EMP Backend/controllers/authController.js
+++ EMP Backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const SuperAdmin = require("../models/SuperAdminModel");
+const Company = require('../models/CoAdminSchema')
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const secret = "suraj123"
@@ -59,7 +60,47 @@ const LoginSuperAdmin = async (req, res) => {
   })
 };
 
+// Company Login
+
+const LoginCompany=async(req,res)=>{
+  let success = false
+  const {email,password} = req.body;
+  const existingCompany = await Company.findOne({email})
+  if(!existingCompany){
+    return res.status(401).json({
+      data:success,
+      message:"Company already exists"
+    })
+  }
+  const passwordCompare = await bcrypt.compare(
+    password,
+    existingCompany.password
+  );
+  if (!passwordCompare) {
+    return res.status(400).json({
+      message: "Invalid Credentials",
+    });
+  }
+
+  success = true;
+  const data = {
+    company:{
+        id:existingCompany.id
+    }
+  }
+  const token = jwt.sign(data,secret);
+  res.status(200).json({
+    data:success,
+    token:token,
+    userRole:"Company"
+  })
+}
+
+
+
+
 module.exports = {
   RegisterSuperAdmin,
-  LoginSuperAdmin
+  LoginSuperAdmin,
+  LoginCompany
 };
--- EMP Backend/routes/authRoutes.js
+++ EMP Backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {RegisterSuperAdmin,LoginSuperAdmin} = require('../controllers/authController');
+const {RegisterSuperAdmin,LoginSuperAdmin,LoginCompany} = require('../controllers/authController');
 
 
 router.post('/registerSuperAdmin',RegisterSuperAdmin)
 router.post('/loginSuperAdmin',LoginSuperAdmin)
+router.post('/loginCompany',LoginCompany)
 
 module.exports = router;
